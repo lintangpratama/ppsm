67bcd90dabde2f932ab707fbfa763bdb43f73b60
Lintang Aji Yoga Pratama <lintangajiyogapratama@gmail.com>
2022-11-17 22:54:00 +0700

    update

--- src/pages/AllEvents.vue
+++ src/pages/AllEvents.vue
@@ -1,5 +1,5 @@
 <template>
-  <Navbar />
+  <NavbarUser />
   <div class="bg-white">
     <div class="max-w-2xl mx-auto py-16 px-4 sm:py-16 sm:px-6 lg:max-w-7xl lg:px-8">
       <h2 class="text-2xl mb-10 font-extrabold tracking-tight text-gray-900">
@@ -51,7 +51,7 @@
 </template>
 
 <script setup>
-import Navbar from "../components/Navbar.vue";
+import NavbarUser from "../components/NavbarUser.vue";
 </script>
 
 <script>
